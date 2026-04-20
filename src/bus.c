7bc4edbf6bbf3d518c03a73c531406fc5fab0209
Daniel <dolomountd@gmail.com>
2026-04-19 23:23:20 -0230

    Bus functions for read/write

--- src/bus.c
+++ src/bus.c
@@ -1,4 +1,5 @@
-#include "bus.h"
+#include "../include/bus.h"
+#include <signal.h>
 
 void bus_init(bus_t *bus, Memory_t *memory, Cartridge_t *cartridge)
 {
@@ -50,3 +51,58 @@ uint8_t bus_read8(bus_t *bus, uint16_t addr)
 
     return 0xFF;
 }
+
+uint16_t bus_read16(bus_t *bus, uint16_t addr)
+{
+    uint16_t value = 0;
+
+    // little-endian
+    value |= bus_read8(bus, addr);
+    addr++;
+    value |= (bus_read8(bus, addr) << 8);
+
+    return value;
+}
+
+void bus_write8(bus_t *bus, uint16_t addr, uint8_t value)
+{
+    // Write to VRAM
+    if (addr >= VRAM_START_ADDR && addr <= VRAM_END_ADDR)
+    {
+        bus->memory->VRAM[addr - VRAM_START_ADDR] = value;
+    }
+
+    // Write to WRAM
+    if (addr >= WRAM_START_ADDR && addr <= WRAM_END_ADDR)
+    {
+        bus->memory->WRAM[addr - WRAM_START_ADDR] = value;
+    }
+
+    // Write to OAM
+    if (addr >= OAM_START_ADDR && addr <= OAM_END_ADDR)
+    {
+        bus->memory->OAM[addr - OAM_START_ADDR] = value;
+    }
+
+    // Write to HRAM
+    if (addr >= HRAM_START_ADDR && addr <= HRAM_END_ADDR)
+    {
+        bus->memory->HRAM[addr - HRAM_START_ADDR] = value;
+    }
+
+    if (addr == IE_ADDR)
+    {
+        bus->memory->ie = value;
+    }
+}
+
+void bus_write16(bus_t *bus, uint16_t addr, uint16_t value)
+{
+    uint8_t low_byte = (uint8_t)(value & 0xFF);
+    uint8_t high_byte = (uint8_t)((value >> 4) & 0xFF);
+
+    bus_write8(bus, addr, low_byte);
+    addr++;
+    bus_write8(bus, addr, high_byte);
+}
+
