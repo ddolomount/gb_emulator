307f63f2284b91e481e459d50bec6e890961ecb6
Daniel <dolomountd@gmail.com>
2026-04-19 23:23:56 -0230

    ld [r16mem], a

--- src/cpu.c
+++ src/cpu.c
@@ -1,6 +1,106 @@
+#include <signal.h>
 #include <string.h>
 #include "../include/cpu.h"
 
+/* Static Functions */
+
+
+
+static void cpu_set_r16(cpu_t *cpu, uint8_t reg_id, uint16_t value)
+{
+    switch (reg_id)
+    {
+        case 0:
+        {
+            // BC
+            cpu->b = (value >> 8) & 0xFF;
+            cpu->c = value & 0xFF;
+            break;
+        }
+        case 1:
+        {
+            // DE
+            cpu->d = (value >> 8) & 0xFF;
+            cpu->e = value & 0xFF;
+            break;
+        }
+        case 2:
+        {
+            // HL
+            cpu->h = (value >> 8) & 0xFF;
+            cpu->l = value & 0xFF;
+            break;
+        }
+        case 3:
+        {
+            // SP
+            cpu->sp = value;
+            break;
+        }
+    }
+}
+
+static uint16_t cpu_get_r16(cpu_t *cpu, uint8_t reg_id)
+{
+    switch (reg_id)
+    {
+        case 0:
+        {
+            return ((uint16_t)cpu->b << 8) | cpu->c; // BC
+        }
+        case 1:
+        {
+            return ((uint16_t)cpu->d << 8) | cpu->e; // DE
+        }
+        case 2:
+        {
+            return ((uint16_t)cpu->h << 8) | cpu->l; // HL
+        }
+        case 3:
+        {
+            return cpu->sp; // SP
+        }
+        default:
+        {
+            return 0;
+        }
+    }
+}
+
+static uint16_t cpu_get_r16mem(cpu_t *cpu, uint8_t reg_id)
+{
+    switch (reg_id)
+    {
+        case 0:
+        {
+            return ((uint16_t)cpu->b << 8) | cpu->c; // BC
+        }
+        case 1:
+        {
+            return ((uint16_t)cpu->d << 8) | cpu->e; // DE
+        }
+        case 2:
+        {
+            // hl+
+            uint16_t hl = ((uint16_t)cpu->h << 8) | cpu->l;
+            cpu_set_r16(cpu, 2, hl + 1);
+            return hl;
+        }
+        case 3:
+        {
+            // hl-
+            uint16_t hl = ((uint16_t)cpu->h << 8) | cpu->l;
+            cpu_set_r16(cpu, 2, hl - 1);
+            return hl;
+        }
+        default:
+        {
+            return 0;
+        }
+    }
+}
+
+/* Global Functions */
 
 void cpu_init(cpu_t *cpu)
 {
@@ -20,9 +120,37 @@ uint8_t cpu_step(cpu_t *cpu, bus_t *bus)
     // Decode instruction and execute
     switch (opcode) 
     {
+        // nop
         case 0x00:
         {
             cycle_count = 4;
+            break;
+        }
+        // ld r16, imm16
+        case 0x01:
+        case 0x11:
+        case 0x21:
+        case 0x31:
+        {
+            // Read imm16 from cartridge and write to register
+            uint16_t value = bus_read16(bus, cpu->pc);
+            cpu->pc += 2;
+
+            cpu_set_r16(cpu, ((opcode >> 4) & 0x03), value);
+
+            cycle_count = 12;
+            break;
+        }
+        case 0x02:
+        case 0x12:
+        case 0x22:
+        case 0x32:
+        {
+            // Read 16-bit mem address and write A to it
+            uint16_t r16mem = cpu_get_r16mem(cpu, ((opcode >> 4) & 0x03));
+            bus_write8(bus, r16mem, cpu->a);
+
+            cycle_count = 8;
             break;
         }
     }
@@ -30,3 +158,7 @@ uint8_t cpu_step(cpu_t *cpu, bus_t *bus)
     // Return number of cycles required for instruction
     return cycle_count;
 }
+
+
+
+
--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -59,10 +59,323 @@ void test_cpu_nop_instruction(void)
     TEST_ASSERT_EQUAL_UINT16(0, cpu.sp);
 }
 
+static void setup_instruction_test(uint8_t *rom,
+                                   size_t rom_size,
+                                   Memory_t *memory,
+                                   Cartridge_t *cartridge,
+                                   bus_t *bus,
+                                   uint8_t opcode,
+                                   uint8_t imm_lo,
+                                   uint8_t imm_hi)
+{
+    rom[0x0100] = opcode;
+    rom[0x0101] = imm_lo;
+    rom[0x0102] = imm_hi;
+
+    *cartridge = (Cartridge_t){
+        .rom = rom,
+        .rom_size = rom_size,
+    };
+
+    *memory = (Memory_t){0};
+
+    bus_init(bus, memory, cartridge);
+}
+
+static void setup_opcode_test(uint8_t *rom,
+                              size_t rom_size,
+                              Memory_t *memory,
+                              Cartridge_t *cartridge,
+                              bus_t *bus,
+                              uint8_t opcode)
+{
+    setup_instruction_test(rom, rom_size, memory, cartridge, bus, opcode, 0x00, 0x00);
+}
+
+static cpu_t test_cpu_with_sentinel_registers(void)
+{
+    return (cpu_t){
+        .a = 0xA1,
+        .f = 0xF0,
+        .b = 0xB1,
+        .c = 0xC1,
+        .d = 0xD1,
+        .e = 0xE1,
+        .h = 0xA5,
+        .l = 0x5A,
+        .sp = 0xDFFE,
+        .pc = 0x0100,
+    };
+}
+
+static void assert_ld_r16_common_result(cpu_t *cpu, uint8_t cycles)
+{
+    TEST_ASSERT_EQUAL_UINT8(12, cycles);
+    TEST_ASSERT_EQUAL_UINT16(0x0103, cpu->pc);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA1, cpu->a);
+    TEST_ASSERT_EQUAL_UINT8(0xF0, cpu->f);
+}
+
+void test_ld_bc_imm16(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_instruction_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x01, 0x34, 0x12);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0x12, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0x34, cpu.c);
+
+    TEST_ASSERT_EQUAL_UINT8(0xD1, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xE1, cpu.e);
+    TEST_ASSERT_EQUAL_UINT8(0xA5, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x5A, cpu.l);
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
+void test_ld_de_imm16(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_instruction_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x11, 0xCD, 0xAB);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xB1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.c);
+
+    TEST_ASSERT_EQUAL_UINT8(0xAB, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xCD, cpu.e);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA5, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x5A, cpu.l);
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
+void test_ld_hl_imm16(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_instruction_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x21, 0xEF, 0xBE);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xB1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.c);
+    TEST_ASSERT_EQUAL_UINT8(0xD1, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xE1, cpu.e);
+
+    TEST_ASSERT_EQUAL_UINT8(0xBE, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0xEF, cpu.l);
+
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
+void test_ld_sp_imm16(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_instruction_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x31, 0xFE, 0xFF);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xB1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.c);
+    TEST_ASSERT_EQUAL_UINT8(0xD1, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xE1, cpu.e);
+    TEST_ASSERT_EQUAL_UINT8(0xA5, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x5A, cpu.l);
+
+    TEST_ASSERT_EQUAL_UINT16(0xFFFE, cpu.sp);
+}
+
+static void assert_ld_r16mem_a_common_result(cpu_t *cpu, uint8_t cycles)
+{
+    TEST_ASSERT_EQUAL_UINT8(8, cycles);
+    TEST_ASSERT_EQUAL_UINT16(0x0101, cpu->pc);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA1, cpu->a);
+    TEST_ASSERT_EQUAL_UINT8(0xF0, cpu->f);
+}
+
+void test_ld_bc_mem_a(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_opcode_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x02);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+    cpu.b = 0xC1;
+    cpu.c = 0x23;
+
+    memory.WRAM[0x0122] = 0x44;
+    memory.WRAM[0x0123] = 0x00;
+    memory.WRAM[0x0124] = 0x55;
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16mem_a_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA1, memory.WRAM[0x0123]);
+    TEST_ASSERT_EQUAL_UINT8(0x44, memory.WRAM[0x0122]);
+    TEST_ASSERT_EQUAL_UINT8(0x55, memory.WRAM[0x0124]);
+
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0x23, cpu.c);
+    TEST_ASSERT_EQUAL_UINT8(0xD1, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xE1, cpu.e);
+    TEST_ASSERT_EQUAL_UINT8(0xA5, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x5A, cpu.l);
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
+void test_ld_de_mem_a(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_opcode_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x12);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+    cpu.d = 0xC4;
+    cpu.e = 0x56;
+
+    memory.WRAM[0x0455] = 0x66;
+    memory.WRAM[0x0456] = 0x00;
+    memory.WRAM[0x0457] = 0x77;
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16mem_a_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA1, memory.WRAM[0x0456]);
+    TEST_ASSERT_EQUAL_UINT8(0x66, memory.WRAM[0x0455]);
+    TEST_ASSERT_EQUAL_UINT8(0x77, memory.WRAM[0x0457]);
+
+    TEST_ASSERT_EQUAL_UINT8(0xB1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.c);
+    TEST_ASSERT_EQUAL_UINT8(0xC4, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0x56, cpu.e);
+    TEST_ASSERT_EQUAL_UINT8(0xA5, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x5A, cpu.l);
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
+void test_ld_hli_mem_a(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_opcode_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x22);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+    cpu.h = 0xC7;
+    cpu.l = 0x89;
+
+    memory.WRAM[0x0788] = 0x88;
+    memory.WRAM[0x0789] = 0x00;
+    memory.WRAM[0x078A] = 0x99;
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16mem_a_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA1, memory.WRAM[0x0789]);
+    TEST_ASSERT_EQUAL_UINT8(0x88, memory.WRAM[0x0788]);
+    TEST_ASSERT_EQUAL_UINT8(0x99, memory.WRAM[0x078A]);
+
+    TEST_ASSERT_EQUAL_UINT8(0xB1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.c);
+    TEST_ASSERT_EQUAL_UINT8(0xD1, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xE1, cpu.e);
+    TEST_ASSERT_EQUAL_UINT8(0xC7, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x8A, cpu.l);
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
+void test_ld_hld_mem_a(void)
+{
+    uint8_t rom[0x200] = {0};
+    Memory_t memory;
+    Cartridge_t cartridge;
+    bus_t bus;
+
+    setup_opcode_test(rom, sizeof(rom), &memory, &cartridge, &bus, 0x32);
+
+    cpu_t cpu = test_cpu_with_sentinel_registers();
+    cpu.h = 0xC7;
+    cpu.l = 0x89;
+
+    memory.WRAM[0x0788] = 0xAA;
+    memory.WRAM[0x0789] = 0x00;
+    memory.WRAM[0x078A] = 0xBB;
+
+    uint8_t cycles = cpu_step(&cpu, &bus);
+
+    assert_ld_r16mem_a_common_result(&cpu, cycles);
+
+    TEST_ASSERT_EQUAL_UINT8(0xA1, memory.WRAM[0x0789]);
+    TEST_ASSERT_EQUAL_UINT8(0xAA, memory.WRAM[0x0788]);
+    TEST_ASSERT_EQUAL_UINT8(0xBB, memory.WRAM[0x078A]);
+
+    TEST_ASSERT_EQUAL_UINT8(0xB1, cpu.b);
+    TEST_ASSERT_EQUAL_UINT8(0xC1, cpu.c);
+    TEST_ASSERT_EQUAL_UINT8(0xD1, cpu.d);
+    TEST_ASSERT_EQUAL_UINT8(0xE1, cpu.e);
+    TEST_ASSERT_EQUAL_UINT8(0xC7, cpu.h);
+    TEST_ASSERT_EQUAL_UINT8(0x88, cpu.l);
+    TEST_ASSERT_EQUAL_UINT16(0xDFFE, cpu.sp);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
     RUN_TEST(test_cpu_init_sets_registers_to_zero);
     RUN_TEST(test_cpu_nop_instruction);
+    RUN_TEST(test_ld_bc_imm16);
+    RUN_TEST(test_ld_de_imm16);
+    RUN_TEST(test_ld_hl_imm16);
+    RUN_TEST(test_ld_sp_imm16);
+    RUN_TEST(test_ld_bc_mem_a);
+    RUN_TEST(test_ld_de_mem_a);
+    RUN_TEST(test_ld_hli_mem_a);
+    RUN_TEST(test_ld_hld_mem_a);
     return UNITY_END();
 }
